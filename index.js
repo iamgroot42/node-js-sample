f831559b44b183069e81a323b9a402cd640f4a50
Anshuman Suri <anshuman14021@iiitd.ac.in>
2017-07-25 00:19:02 +0530

    Linked proxy

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-var express = require('express')
-var app = express()
+var http = require('http'),
+    httpProxy = require('http-proxy');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+httpProxy.createProxyServer({target:'http://laiwyer.eastus2.cloudapp.azure.com/'}).listen(1024);
