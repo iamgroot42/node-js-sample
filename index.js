b7b943a86e5a45edd1ad9350ab17bf2db768a155
Anshuman Suri <anshuman14021@iiitd.ac.in>
2017-07-25 00:29:56 +0530

    Final heroku push

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var http = require('http'),
     httpProxy = require('http-proxy');
 
-httpProxy.createProxyServer({target:'http://laiwyer.eastus2.cloudapp.azure.com/'}).listen(process.env.PORT || 1024);
+httpProxy.createProxyServer({target:'http://laiwyer.eastus2.cloudapp.azure.com/botapi'}).listen(process.env.PORT || 1024);
